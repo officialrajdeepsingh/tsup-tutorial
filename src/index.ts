d31a94037f772dce956a7d3a932826236bc97176
officialrajdeepsingh <radewala.2@gmail.com>
2023-06-20 19:34:18 +0530

    delete the unsed code

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import type { AddNum, AddString } from "./types";
-
-export function Add(num: AddNum, num2: AddNum): AddNum {
-  return num + num2;
-}
-
-export function ConCat(FirstItem: AddString, SecondItem: AddString): AddString {
-  return FirstItem + SecondItem;
-}
