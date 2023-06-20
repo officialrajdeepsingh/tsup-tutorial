6479dec66543c071702d416eb6da32e0e3c8b225
officialrajdeepsingh <radewala.2@gmail.com>
2023-06-20 19:35:10 +0530

    tsup config

---
+++ tsup.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "tsup";
+import tsconfig from "./tsconfig.json";
+
+export default defineConfig({
+  entry: ["src/index.tsx", "src/Button.tsx","src/Headings.tsx","src/Icons.tsx"],
+  dts: true,
+  format: ["esm","cjs"],
+  name: "tsup-tutorial",
+  outExtension: () => ({ js: ".js" }),
+  target: tsconfig.compilerOptions.target as "es2016",
+  external: ["nextra"],
+});
