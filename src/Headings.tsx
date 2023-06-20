59d6dddafea885ae7b6eae1cafa53d51b8004d65
officialrajdeepsingh <radewala.2@gmail.com>
2023-06-20 19:33:55 +0530

    Headings component

---
+++ src/Headings.tsx
@@ -0,0 +1,18 @@
+export function H1(props) {
+    return (
+        <h1 className="text-5xl font-extrabold dark:text-white">{props.children}</h1>
+    );
+}
+
+export function H2(props) {
+    return (
+        <h1 className="text-4xl font-extrabold dark:text-white">{props.children}</h1>
+    );
+}
+
+export function H3(props) {
+    return (
+        <h1 className="text-3xl font-extrabold dark:text-white">{props.children}</h1>
+    );
+}
+
