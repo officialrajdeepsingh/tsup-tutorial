31d1ff2256186f32471694853b735d3bfb40b328
officialrajdeepsingh <radewala.2@gmail.com>
2023-06-21 23:47:22 +0530

    fix the heading

--- src/Headings.tsx
+++ src/Headings.tsx
@@ -1,18 +1,11 @@
-export function H1(props) {
-    return (
-        <h1 className="text-5xl font-extrabold dark:text-white">{props.children}</h1>
-    );
+export function H1({ text }: { text: string }) {
+  return <h1 className="text-5xl font-extrabold dark:text-white"> {text} </h1>;
 }
 
-export function H2(props) {
-    return (
-        <h1 className="text-4xl font-extrabold dark:text-white">{props.children}</h1>
-    );
+export function H2({ text }: { text: string }) {
+  return <h2 className="text-4xl font-extrabold dark:text-white">{text}</h2>;
 }
 
-export function H3(props) {
-    return (
-        <h1 className="text-3xl font-extrabold dark:text-white">{props.children}</h1>
-    );
+export function H3({ text }: { text: string }) {
+  return <h3 className="text-3xl font-extrabold dark:text-white">{text}</h3>;
 }
-
