af6b77a0b12f7c4c094821713a2c2736bea7eb5c
officialrajdeepsingh <radewala.2@gmail.com>
2023-06-23 19:27:07 +0530

    remove css

--- src/Button.tsx
+++ src/Button.tsx
@@ -2,7 +2,7 @@ import type { buttonType } from "./types";
 
 export function Button(props: buttonType) {
   return (
-    <button className="text-white bg-[#3b5998] hover:bg-[#3b5998]/90 focus:ring-4 focus:outline-none focus:ring-[#3b5998]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#3b5998]/55 mr-2 mb-2">
+    <button className="text-white bg-blue font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 mb-2">
       {props.text}
     </button>
   );
