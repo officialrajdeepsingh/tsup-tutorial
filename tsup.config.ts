1393b21bb3414cd417144b00343234c51f563cc6
officialrajdeepsingh <radewala.2@gmail.com>
2023-06-23 22:18:46 +0530

    adding more config in tsup config file

--- tsup.config.ts
+++ tsup.config.ts
@@ -11,6 +11,13 @@ export default defineConfig({
   dts: true,
   format: ["esm", "cjs"],
   name: "tsup-tutorial",
-  outExtension: () => ({ js: ".js" }),
+  splitting: false,
+  outExtension({ format }) {
+    return {
+      js: `.${format}.js`,
+    };
+  },
+  sourcemap: true,
+  clean: true,
   target: tsconfig.compilerOptions.target as "es2016",
 });
