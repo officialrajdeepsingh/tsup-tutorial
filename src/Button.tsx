654578320ba6fda9a596d5960a93ed56dea80977
officialrajdeepsingh <radewala.2@gmail.com>
2023-06-24 13:56:29 +0530

    Fix the button

--- src/Button.tsx
+++ src/Button.tsx
@@ -3,7 +3,7 @@ import type { buttonType } from "./types";
 export function Button(props: buttonType) {
   return (
     <button className="text-white bg-blue font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 mb-2">
-      {props.text}
+      {props.children}
     </button>
   );
 }
