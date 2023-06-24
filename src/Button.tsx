87f4c03bd261fdff17f0a22f431dd88a4a851317
officialrajdeepsingh <radewala.2@gmail.com>
2023-06-24 18:14:51 +0530

    improve the UI

--- src/Button.tsx
+++ src/Button.tsx
@@ -2,7 +2,7 @@ import type { buttonType } from "./types";
 
 export function Button(props: buttonType) {
   return (
-    <button className="text-white bg-blue font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 mb-2">
+    <button className="text-white bg-blue-500 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 mb-2">
       {props.children}
     </button>
   );
