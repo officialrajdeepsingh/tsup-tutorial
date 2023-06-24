5285263d0339ea6775728a7c4c2ce9f8f0dd43c3
officialrajdeepsingh <radewala.2@gmail.com>
2023-06-24 14:02:52 +0530

    adding navItem and logo

--- src/index.tsx
+++ src/index.tsx
@@ -10,15 +10,19 @@ export function Header(props: headerProps) {
         
         <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
           <button type="button" className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
-            <span className="sr-only">Open main menu</span>
+            <span className="sr-only"> Open main menu </span>
 
-            <svg className="block h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
+            {
+              logo? logo: <>  <svg className="block h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
               <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
             </svg>
 
             <svg className="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
               <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-            </svg>
+            </svg> 
+            </>
+            }
+
           </button>
         </div>
         <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
@@ -28,11 +32,10 @@ export function Header(props: headerProps) {
           </div>
           <div className="hidden sm:ml-6 sm:block">
             <div className="flex space-x-4">
-              <a href="#" className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Dashboard</a>
-              <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Team</a>
-              <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Projects</a>
-              <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Calendar</a>
-            </div>
+              {
+                navItem.map(item=><a href={item.url} className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">{item.name}</a>)
+              }
+             </div>
           </div>
         </div>
 
@@ -62,10 +65,7 @@ export function Header(props: headerProps) {
   
     <div className="sm:hidden" id="mobile-menu">
       <div className="space-y-1 px-2 pb-3 pt-2">
-        <a href="#" className="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium" aria-current="page">Dashboard</a>
-        <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Team</a>
-        <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Projects</a>
-        <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Calendar</a>
+          { navItem.map(item=><a href={item.url} className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">{item.name}</a>)}
       </div>
     </div>
   </nav>
