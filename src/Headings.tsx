87faf24f108060a4f508b39165114cf2972d7ea4
officialrajdeepsingh <radewala.2@gmail.com>
2023-06-24 18:15:13 +0530

    improve the UI and remove the dark mode

--- src/Headings.tsx
+++ src/Headings.tsx
@@ -1,19 +1,13 @@
 import type { ReactNode } from "react";
 
 export function H1({ children }: { children: ReactNode }) {
-  return (
-    <h1 className="text-5xl font-extrabold dark:text-white"> {children} </h1>
-  );
+  return <h1 className="text-5xl text-black font-extrabold"> {children} </h1>;
 }
 
 export function H2({ children }: { children: ReactNode }) {
-  return (
-    <h2 className="text-4xl font-extrabold dark:text-white">{children}</h2>
-  );
+  return <h2 className="text-4xl text-black  font-extrabold">{children}</h2>;
 }
 
 export function H3({ children }: { children: ReactNode }) {
-  return (
-    <h3 className="text-3xl font-extrabold dark:text-white">{children}</h3>
-  );
+  return <h3 className="text-3xl text-black  font-extrabold">{children}</h3>;
 }
